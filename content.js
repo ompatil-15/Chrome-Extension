af8ce6737d979e24ea01354c985aed90571f6a37
Om Patil <patilom001@gmail.com>
2024-09-22 18:09:53 +0530

    Fixed minor issues

--- content.js
+++ content.js
@@ -1,4 +1,11 @@
-import { generateStrongPassword } from './utils.js';
+function generateStrongPassword() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;';
+  let password = '';
+  for (let i = 0; i < 25; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+}
 
 function createHoverCard(password, inputElement) {
   const hoverCard = document.createElement('div');
--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
-import { generateStrongPassword } from './utils.js';
+function generateStrongPassword() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;';
+    let password = '';
+    for (let i = 0; i < 25; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const passwordField = document.getElementById('generatedPassword');
